ca5ae142abe702be250af958a3ea5c1621452d01
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2025-11-02 21:19:36 +0900

    feat: 구입금액 공백 입력 예외 처리 추가

--- src/main/java/lotto/domain/PurchaseAmount.java
+++ src/main/java/lotto/domain/PurchaseAmount.java
@@ -1,11 +1,23 @@
 package lotto.domain;
 
 import java.math.BigInteger;
+import lotto.domain.exception.ExceptionMessage;
 
 public class PurchaseAmount {
     private final BigInteger value;
 
     public PurchaseAmount(String rawPurchaseAmount) {
+        validate(rawPurchaseAmount);
         this.value = new BigInteger(rawPurchaseAmount);
+    }
+
+    private void validate(String rawPurchaseAmount) {
+        validateEmpty(rawPurchaseAmount);
+    }
+
+    private void validateEmpty(String rawPurchaseAmount) {
+        if (rawPurchaseAmount == null || rawPurchaseAmount.isBlank()) {
+            throw new IllegalArgumentException(ExceptionMessage.PURCHASE_AMOUNT_IS_EMPTY.getMessage());
+        }
     }
 }
