4b9d7d4b1a49a4a68318d77e2366cdea4ee8c33c
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2025-11-03 03:00:40 +0900

    feat: 구입금액만큼 로또 발행 기능 로직 연결

--- src/main/java/lotto/ApplicationConfig.java
+++ src/main/java/lotto/ApplicationConfig.java
@@ -3,14 +3,19 @@ package lotto;
 import lotto.adapter.console.ConsoleInputReader;
 import lotto.adapter.console.ConsoleLottoController;
 import lotto.adapter.console.ConsolePresenter;
+import lotto.adapter.random.WootecoRandomNumberGenerator;
+import lotto.application.LottoDispenserService;
 import lotto.application.LottoOrderService;
+import lotto.application.required.RandomNumberGenerator;
 
 public class ApplicationConfig {
     public ConsoleLottoController createConsoleLottoController() {
         ConsolePresenter consolePresenter = new ConsolePresenter();
         ConsoleInputReader consoleInputReader = new ConsoleInputReader();
         LottoOrderService lottoService = new LottoOrderService();
+        RandomNumberGenerator randomNumberGenerator = new WootecoRandomNumberGenerator();
+        LottoDispenserService lottoDispenserService = new LottoDispenserService(randomNumberGenerator);
 
-        return new ConsoleLottoController(consolePresenter, consoleInputReader, lottoService);
+        return new ConsoleLottoController(consolePresenter, consoleInputReader, lottoService, lottoDispenserService);
     }
 }
--- src/main/java/lotto/adapter/console/ConsoleLottoController.java
+++ src/main/java/lotto/adapter/console/ConsoleLottoController.java
@@ -1,22 +1,27 @@
 package lotto.adapter.console;
 
+import lotto.application.LottoDispenserService;
 import lotto.application.LottoOrderService;
+import lotto.domain.Lottos;
 import lotto.domain.PurchaseAmount;
 
 public class ConsoleLottoController {
     private final ConsolePresenter consolePresenter;
     private final ConsoleInputReader consoleInputReader;
     private final LottoOrderService lottoService;
+    private final LottoDispenserService lottoDispenserService;
 
     public ConsoleLottoController(ConsolePresenter consolePresenter, ConsoleInputReader consoleInputReader,
-                                  LottoOrderService lottoService) {
+                                  LottoOrderService lottoService, LottoDispenserService lottoDispenserService) {
         this.consolePresenter = consolePresenter;
         this.consoleInputReader = consoleInputReader;
         this.lottoService = lottoService;
+        this.lottoDispenserService = lottoDispenserService;
     }
 
     public void run() {
         PurchaseAmount purchaseAmount = getPurchaseAmount();
+        Lottos lottos = getLottos(purchaseAmount);
     }
 
     private PurchaseAmount getPurchaseAmount() {
@@ -29,5 +34,9 @@ public class ConsoleLottoController {
                 consolePresenter.printErrorMessage(e.getMessage());
             }
         }
+    }
+
+    private Lottos getLottos(PurchaseAmount purchaseAmount) {
+        return lottoDispenserService.dispenseLottos(purchaseAmount);
     }
 }
