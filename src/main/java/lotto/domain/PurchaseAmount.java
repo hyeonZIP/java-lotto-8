48dd886684109a30c2598fbe982dcd9d7efe1c99
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2025-11-03 01:18:51 +0900

    feat: 구매금액 한도 초과 예외 처리 추가

--- src/main/java/lotto/domain/PurchaseAmount.java
+++ src/main/java/lotto/domain/PurchaseAmount.java
@@ -5,6 +5,7 @@ import lotto.domain.exception.ExceptionMessage;
 
 public class PurchaseAmount {
     private static final BigInteger LOTTO_PRICE = new BigInteger("1000");
+    private static final BigInteger MAXIMUM_PURCHASE_AMOUNT = new BigInteger("100000000");
 
     private final BigInteger value;
 
@@ -19,8 +20,19 @@ public class PurchaseAmount {
 
         validatePositive(purchaseAmount);
         validateDivisible(purchaseAmount);
+        validateMaximum(purchaseAmount);
 
         return purchaseAmount;
+    }
+
+    private void validateMaximum(BigInteger purchaseAmount) {
+        if (isBiggerThanMaximum(purchaseAmount)) {
+            throw new IllegalArgumentException(ExceptionMessage.PURCHASE_AMOUNT_IS_OVER_MAXIMUM.getMessage());
+        }
+    }
+
+    private boolean isBiggerThanMaximum(BigInteger purchaseAmount) {
+        return purchaseAmount.compareTo(MAXIMUM_PURCHASE_AMOUNT) > 0;
     }
 
     private BigInteger parse(String rawPurchaseAmount) {
