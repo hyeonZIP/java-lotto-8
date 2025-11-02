a0534fcb2fc46ab97b1d81412caa7295c2e3c73e
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2025-11-03 02:43:31 +0900

    refactor: 조건문 메서드로 분리

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -8,7 +8,7 @@ import lotto.domain.exception.ExceptionMessage;
 public class Lotto {
     private static final int MINIMUM_LOTTO_NUMBER = 1;
     private static final int MAXIMUM_LOTTO_NUMBER = 45;
-    private static final int LOTTO_NUMBERS_SIZE = 6;
+    private static final int LOTTO_SIZE = 6;
 
     private final List<Integer> numbers;
 
@@ -24,13 +24,18 @@ public class Lotto {
     }
 
     private void validateLottoNumberRange(List<Integer> numbers) {
-        if (numbers.stream().anyMatch(number -> number < MINIMUM_LOTTO_NUMBER || number > MAXIMUM_LOTTO_NUMBER)) {
+        if (isOutOfLottoNumberRange(numbers)) {
             throw new IllegalArgumentException(ExceptionMessage.LOTTO_INVALID_RANGE.getMessage());
         }
     }
 
+    private boolean isOutOfLottoNumberRange(List<Integer> numbers) {
+        return numbers.stream().anyMatch(
+                number -> number < MINIMUM_LOTTO_NUMBER || number > MAXIMUM_LOTTO_NUMBER);
+    }
+
     private void validateLottoNumbersCount(List<Integer> numbers) {
-        if (numbers.size() != LOTTO_NUMBERS_SIZE) {
+        if (numbers.size() != LOTTO_SIZE) {
             throw new IllegalArgumentException(ExceptionMessage.LOTTO_INVALID_SIZE.getMessage());
         }
     }
