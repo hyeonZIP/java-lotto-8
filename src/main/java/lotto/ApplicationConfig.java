4c130cbee3694a8802261a2a941189f0a3454ab6
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2025-11-02 22:48:41 +0900

    feat: 구입금액 입력 로직 연결

--- src/main/java/lotto/ApplicationConfig.java
+++ src/main/java/lotto/ApplicationConfig.java
@@ -3,12 +3,14 @@ package lotto;
 import lotto.adapter.console.ConsoleInputReader;
 import lotto.adapter.console.ConsoleLottoController;
 import lotto.adapter.console.ConsolePresenter;
+import lotto.application.LottoOrderService;
 
 public class ApplicationConfig {
     public ConsoleLottoController createConsoleLottoController() {
         ConsolePresenter consolePresenter = new ConsolePresenter();
         ConsoleInputReader consoleInputReader = new ConsoleInputReader();
+        LottoOrderService lottoService = new LottoOrderService();
 
-        return new ConsoleLottoController(consolePresenter, consoleInputReader);
+        return new ConsoleLottoController(consolePresenter, consoleInputReader, lottoService);
     }
 }
--- src/main/java/lotto/adapter/console/ConsoleLottoController.java
+++ src/main/java/lotto/adapter/console/ConsoleLottoController.java
@@ -1,16 +1,23 @@
 package lotto.adapter.console;
 
+import lotto.application.LottoOrderService;
+import lotto.domain.PurchaseAmount;
+
 public class ConsoleLottoController {
     private final ConsolePresenter consolePresenter;
     private final ConsoleInputReader consoleInputReader;
+    private final LottoOrderService lottoService;
 
-    public ConsoleLottoController(ConsolePresenter consolePresenter, ConsoleInputReader consoleInputReader) {
+    public ConsoleLottoController(ConsolePresenter consolePresenter, ConsoleInputReader consoleInputReader,
+                                  LottoOrderService lottoService) {
         this.consolePresenter = consolePresenter;
         this.consoleInputReader = consoleInputReader;
+        this.lottoService = lottoService;
     }
 
     public void run() {
         consolePresenter.printPurchaseAmountGuide();
         String rawPurchaseAmount = consoleInputReader.getConsoleInput();
+        PurchaseAmount purchaseAmount = lottoService.orderPurchaseAmount(rawPurchaseAmount);
     }
 }
