4f16f6dd6d4bc6ded3a0bcdc0e792b76a0a695b9
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2025-11-03 18:07:38 +0900

    feat: 당첨 번호 검증 기능 추가

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -1,11 +1,33 @@
 package lotto.domain;
 
 import java.util.List;
+import lotto.application.required.WinningNumberExtractor;
+import lotto.domain.exception.ExceptionMessage;
 
 public class WinningLotto {
     private final Lotto lotto;
 
     public WinningLotto(List<Integer> winningNumbers) {
         this.lotto = Lotto.createWinningLotto(winningNumbers);
+    }
+
+    public static WinningLotto of(String winningNumbers, WinningNumberExtractor extractor) {
+        validateBlank(winningNumbers);
+
+        return new WinningLotto(extract(winningNumbers, extractor));
+    }
+
+    private static List<Integer> extract(String winningNumbers, WinningNumberExtractor extractor) {
+        try {
+            return extractor.extract(winningNumbers);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ExceptionMessage.WINNING_NUMBERS_IS_NOT_DIGIT.getMessage());
+        }
+    }
+
+    private static void validateBlank(String winningNumbers) {
+        if (winningNumbers == null || winningNumbers.isBlank()) {
+            throw new IllegalArgumentException(ExceptionMessage.WINNING_NUMBERS_IS_BLANK.getMessage());
+        }
     }
 }
