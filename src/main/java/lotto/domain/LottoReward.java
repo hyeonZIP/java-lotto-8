8091eebb3be1f88e036fae132119b1958ba84296
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2025-11-03 20:23:42 +0900

    feat: 로또 당첨 금액 정보 관리 객체 추가

---
+++ src/main/java/lotto/domain/LottoReward.java
@@ -0,0 +1,36 @@
+package lotto.domain;
+
+import java.util.Arrays;
+
+public enum LottoReward {
+    NONE(0, false, 0),
+    FIFTH(3, false, 5_000),
+    FOURTH(4, false, 50_000),
+    THIRD(5, false, 1_500_000),
+    SECOND(5, true, 30_000_000),
+    FIRST(6, false, 2_000_000_000),
+    ;
+
+    private int matchCount;
+    private boolean hasBonusNumber;
+    private int reward;
+
+    LottoReward(int matchCount, boolean hasBonusNumber, int reward) {
+        this.matchCount = matchCount;
+        this.hasBonusNumber = hasBonusNumber;
+        this.reward = reward;
+    }
+
+    public static LottoReward of(int matchCount, boolean hasBonusNumber) {
+        return Arrays.stream(values())
+                .filter(lottoReward ->
+                        lottoReward.matchCount == matchCount
+                                && lottoReward.hasBonusNumber == hasBonusNumber)
+                .findFirst()
+                .orElse(NONE);
+    }
+
+    public int getReward() {
+        return reward;
+    }
+}
