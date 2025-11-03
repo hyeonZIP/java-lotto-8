6b5f17112a58c207f24890627134b1ba6508a551
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2025-11-03 16:37:04 +0900

    feat: 발행한 로또 번호 출력 기능 추가

--- src/main/java/lotto/adapter/console/ConsolePresenter.java
+++ src/main/java/lotto/adapter/console/ConsolePresenter.java
@@ -1,10 +1,14 @@
 package lotto.adapter.console;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import lotto.adapter.console.dto.LottoDispenserResponse;
 
 public class ConsolePresenter {
     private static final String PURCHASE_AMOUNT_GUIDE = "구입금액을 입력해 주세요.";
     private static final String DISPENSED_LOTTO_COUNT_GUIDE_FORMAT = "\n%s개를 구매했습니다.\n";
+    private static final String DISPENSED_LOTTO_NUMBERS_DETAIL_FORMAT = "[%s]\n";
+    private static final String COMMA_DELIMITER = ", ";
 
     public void printPurchaseAmountGuide() {
         System.out.println(PURCHASE_AMOUNT_GUIDE);
@@ -16,5 +20,16 @@ public class ConsolePresenter {
 
     public void printLottoDispenserResult(LottoDispenserResponse response) {
         System.out.printf(DISPENSED_LOTTO_COUNT_GUIDE_FORMAT, response.lottos().size());
+
+        response.lottos().forEach(o -> printNumbersDetail(o.numbers()));
+    }
+
+    private void printNumbersDetail(List<Integer> numbers) {
+        String numbersDetail = numbers.stream()
+                .sorted()
+                .map(String::valueOf)
+                .collect(Collectors.joining(COMMA_DELIMITER));
+
+        System.out.printf(DISPENSED_LOTTO_NUMBERS_DETAIL_FORMAT, numbersDetail);
     }
 }
